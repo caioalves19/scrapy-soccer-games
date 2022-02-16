daea363017993b42950f516d9e24f07560ccb2f0
caioalves19 <caioluizalves1995@gmail.com>
2022-02-16 12:57:22 -0300

    Revert "inicial"
    This reverts commit e84407108e72f5ea27f301b82dab2af97112a70a.

--- soccer_games/soccer_games/settings.py
+++ soccer_games/soccer_games/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'soccer_games.spiders'
 #USER_AGENT = 'soccer_games (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- soccer_games/soccer_games/spiders/fpf_games.py
+++ soccer_games/soccer_games/spiders/fpf_games.py
@@ -1,53 +1,29 @@
+from lib2to3.pgen2 import driver
 from tracemalloc import start
 from webbrowser import Chrome
 from scrapy.selector import Selector
 import scrapy
-from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver import Chrome
 from shutil import which
 from time import sleep
 
-options = ChromeOptions()
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = Chrome("C:/chromedriver.exe", options=options)
-driver.maximize_window()
 
 class FpfGamesSpider(scrapy.Spider):
     name = 'fpf_games'
     allowed_domains = ['futebolpaulista.com.br']
-    start_urls = []
+    start_urls = ['https://futebolpaulista.com.br/Competicoes/Tabela.aspx?idCampeonato=76&ano=2022&nav=1']
 
     def __init__(self):
-        campeonatos_links = [f'https://futebolpaulista.com.br/Competicoes/Tabela.aspx?idCampeonato={i}&ano=2022&nav=1' for i in [73, 74, 76]]
-        
-        for campeonato_link in campeonatos_links:
-            input('pode? ')
-            driver.get(campeonato_link)
-            sleep(2)
-            try:
-                btn_aceitar = driver.find_element_by_css_selector('.js-aceitar')
-                btn_aceitar.click()
-                sleep(2)
-            except Exception:
-                pass
-            
-            # lista_rodadas = driver.find_elements_by_css_selector('.conteudo #combo-rodadas  li')
-            
-            self.html = driver.page_source
-            # for i in range(len(lista_rodadas)):
-            #     botao_rodadas.click()
-            #     sleep(2)
-            #     lista_rodadas[i+1].click()
-            #     sleep(2)
-            #     btn_impressao = driver.find_element_by_css_selector('.lnk-impressao')
-            #     btn_impressao.click()
-            #     sleep(2)
-            #     self.html = driver.page_source
-            #     print('Passei aqui --------------------------------')
+        driver = Chrome("C:/chromedriver.exe")
+        driver.get("https://futebolpaulista.com.br/Competicoes/Tabela.aspx?idCampeonato=76&ano=2022&nav=1")
+        input('Posso?')
+
+        self.html = driver.page_source
         driver.close()
 
     def parse(self, response):
         resp = Selector(text=self.html)
         for local in resp.css('#placarEstadio'):
             yield{
-                'local': local.css('::text').get().strip()
+                'local': local.css('::text').get()
             }
