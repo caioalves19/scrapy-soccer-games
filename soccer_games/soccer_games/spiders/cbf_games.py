3d3d42b6b55132737525cae59637623ffc66c6a9
caioalves19 <caioluizalves1995@gmail.com>
2022-02-20 14:33:48 -0300

    update

--- soccer_games/soccer_games/spiders/cbf_games.py
+++ soccer_games/soccer_games/spiders/cbf_games.py
@@ -2,20 +2,6 @@ import scrapy
 from scrapy.loader import ItemLoader
 from soccer_games.items import SoccerGamesItem
 
-serie_a = [
-    f'https://www.cbf.com.br/amp/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/2022/00{i+1}'
-    for i in range(380)
-]
-serie_b = [
-    f'https://www.cbf.com.br/amp/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-b/2022/00{i+1}'
-    for i in range(380)
-]
-serie_c = [
-    f'https://www.cbf.com.br/amp/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-c/2022/00{i+1}'
-    for i in range(380)
-]
-
-
 def tratar_hora(hora):
     # Deixar data no padrão do projeto
     if not hora:
@@ -29,9 +15,12 @@ def tratar_data(data):
     return data
 
 
-def rodada_jogo(numero_jogo):
-    rodada = numero_jogo // 10
-    if numero_jogo % 10 != 0:
+def rodada_jogo(nome_campeonato, numero_jogo, numero_jogos = 10):
+    #Usa o número do jogo para descobrir de qual rodada é. O numero_jogos é quantidade de jogos por rodada do campeonato.
+    if 'Copa do Nordeste' in nome_campeonato:
+        numero_jogos = 8
+    rodada = numero_jogo // numero_jogos
+    if numero_jogo % numero_jogos != 0:
         rodada += 1
     return rodada
 
@@ -43,10 +32,34 @@ def tratar_local(local):
     return local
 
 
+def tratar_nome_campeonato(link_nome):
+    if link_nome == 'campeonato-brasileiro-serie-a':
+        return 'Campeonato Brasileiro - Série A'
+    if link_nome == 'campeonato-brasileiro-serie-b':
+        return 'Campeonato Brasileiro - Série B'
+    if link_nome == 'campeonato-brasileiro-serie-c':
+        return 'Campeonato Brasileiro - Série C'
+    if link_nome == 'copa-nordeste-masculino':
+        return 'Copa do Nordeste - Única'
+
+serie_a = [
+    f'https://www.cbf.com.br/amp/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/2022/00{i+1}'
+    for i in range(380)
+]
+serie_b = [
+    f'https://www.cbf.com.br/amp/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-b/2022/00{i+1}'
+    for i in range(380)
+]
+serie_c = [
+    f'https://www.cbf.com.br/amp/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-c/2022/00{i+1}'
+    for i in range(190)
+]
+copa_ne = [f'https://www.cbf.com.br/amp/futebol-brasileiro/competicoes/copa-nordeste-masculino/2022/00{i+1}'
+    for i in range(64)]
 class CbfGamesSpider(scrapy.Spider):
     name = 'cbf_games'
     allowed_domains = ['cbf.com.br']
-    start_urls = serie_a + serie_b + serie_c
+    start_urls = copa_ne
 
     def parse(self, response):
         """
@@ -63,12 +76,7 @@ class CbfGamesSpider(scrapy.Spider):
         data_jogo = tratar_data(response.css('.col-xs-6 span::text').get())
         hora_jogo = tratar_hora(response.css('.col-xs-6 .text-6::text').get())
         numero_jogo = int(response.url[-3::])
-        nome_campeonato = (
-            response.url.split('/')[-3]
-            .replace('-', ' ')
-            .replace('serie', 'série')
-            .title()
-        )
+        nome_campeonato = tratar_nome_campeonato(response.url.split('/')[-3])
 
         jogo.add_css(
             'time_mandante', '.jogo-equipe-nome-completo::text', lambda v: v[0]
@@ -84,7 +92,7 @@ class CbfGamesSpider(scrapy.Spider):
         jogo.add_value('data_jogo', data_jogo.replace('/', '-'))
         jogo.add_value('hora_jogo', hora_jogo)
         jogo.add_value('numero_jogo', numero_jogo)
-        jogo.add_value('rodada_jogo', rodada_jogo(numero_jogo))
+        jogo.add_value('rodada_jogo', rodada_jogo(nome_campeonato, numero_jogo))
         jogo.add_value('nome_campeonato', nome_campeonato)
 
         return jogo.load_item()
