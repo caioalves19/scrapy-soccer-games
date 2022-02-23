f98209064416edc65c477dd2e228a78cf9bd1634
caioalves19 <caioluizalves1995@gmail.com>
2022-02-23 20:58:24 -0300

    configurei serie d

--- soccer_games/soccer_games/spiders/cbf_games.py
+++ soccer_games/soccer_games/spiders/cbf_games.py
@@ -21,6 +21,8 @@ def rodada_jogo(nome_campeonato, numero_jogo, numero_jogos=10):
     # Usa o número do jogo para descobrir de qual rodada é. O numero_jogos é quantidade de jogos por rodada do campeonato.
     if 'Copa do Nordeste' in nome_campeonato:
         numero_jogos = 8
+    if 'Série D' in nome_campeonato:
+        numero_jogos = 32
     rodada = numero_jogo // numero_jogos
     if numero_jogo % numero_jogos != 0:
         rodada += 1
@@ -41,6 +43,8 @@ def tratar_nome_campeonato(link_nome):
         return 'Campeonato Brasileiro - Série B'
     if link_nome == 'campeonato-brasileiro-serie-c':
         return 'Campeonato Brasileiro - Série C'
+    if link_nome == 'campeonato-brasileiro-serie-d':
+        return 'Campeonato Brasileiro - Série D'
     if link_nome == 'copa-nordeste-masculino':
         return 'Copa do Nordeste - Única'
 
@@ -59,16 +63,19 @@ serie_c = [
     f'https://www.cbf.com.br/amp/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-c/2022/00{i+1}'
     for i in range(190)
 ]
+
+serie_d = [f'https://www.cbf.com.br/amp/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-d/2022/00{i+25}'
+    for i in range(10)]
 copa_ne = [
-    f'https://www.cbf.com.br/amp/futebol-brasileiro/competicoes/copa-nordeste-masculino/2022/00{i+44}'
-    for i in range(10)
+    f'https://www.cbf.com.br/amp/futebol-brasileiro/competicoes/copa-nordeste-masculino/2022/00{i+1}'
+    for i in range(68)
 ]
 
 
 class CbfGamesSpider(scrapy.Spider):
     name = 'cbf_games'
     allowed_domains = ['cbf.com.br']
-    start_urls = copa_ne
+    start_urls = serie_d
 
     def parse(self, response):
         """
@@ -84,10 +91,12 @@ class CbfGamesSpider(scrapy.Spider):
         )
         data_jogo = tratar_data(response.css('.col-xs-6 span::text').get())
         data_partida = data_jogo.strip().split('/')
+        
         try:
             data_partida = date(int(data_partida[2]), int(data_partida[1]), int(data_partida[0]))
         except Exception:
             data_partida = date(2023, 1, 1)
+        
         if data_partida < data_hoje:
                 return
         hora_jogo = tratar_hora(response.css('.col-xs-6 .text-6::text').get())
