17d79be416fe01670a2720df22a1bbecdae234cc
caioalves19 <caioluizalves1995@gmail.com>
2022-03-06 16:45:27 -0300

    upgrade

--- soccer_games/soccer_games/items.py
+++ soccer_games/soccer_games/items.py
@@ -3,6 +3,8 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
+from ast import ExceptHandler
+import json
 from itemloaders.processors import Compose, TakeFirst
 from scrapy import Field, Item
 
@@ -13,96 +15,25 @@ def tratar_time(time):
         return 'Selecione um clube'
     # Tratar nomes para ficar de acordo com o projeto
     # Corrigir acentuação
-    nomes_times = {
-        'America': 'América',
-        'Sampaio Correa': 'Sampaio Corrêa',
-        'Boa': 'Boa Esporte',
-        'Clube de Esportes Uniao': 'União ABC',
-        'Minas Brasilia': 'Minas Brasília',
-        'CEU ABC': 'União ABC',
-        'Goianesia': 'Goianésia',
-        'Marilia': 'Marília',
-        'Athletico Paranaense': 'Athletico',
-        'Jc Futebol Clube': 'JC',
-        'Audax': 'Osasco Audax',
-        'Atlético Mineiro': 'Atlético',
-        'Porto Vitória F. C.': 'Porto Vitória',
-        'Sociedade Desportiva Paraense Ltda': 'Desportiva',
-        'Palmas Ltda': 'Palmas',
-        'Liga Presidente M\u00e9dici': 'Presidente Médici',
-        'Confianca': 'Confiança',
-        'Boca Junior': 'Boca Júnior',
-        'São Bernardo FC': 'São Bernardo',
-        'Grêmio Novorizontino': 'Novorizontino',
-        'Inter Limeira': 'Inter de Limeira',
-        'Portuguesa Desp': 'Portuguesa',
-        'XV Piracicaba': 'XV de Piracicaba',
-        'AA Portuguesa': 'Portuguesa Santista',
-        'Comercial RP': 'Comercial',
-        'Suzano': 'União Suzano',
-        'São José EC': 'São José',
-        'Bandeirante EC': 'Bandeirante',
-        'Gremio Anapolis': 'Grêmio Anápolis', 'Nova Venecia F. C.': 'Nova Venécia', 'Marica': 'Maricá', 'Gloria': 'Glória',
-        'Vasco da Gama': 'Vasco', 'Real Noroeste F. C.': 'Real Noroeste',
-        'Associação Atlética Esmac Ananindeua': 'Esmac', 'Cruzeiro Saf': 'Cruzeiro',
-        'Estádio Beira Rio': 'Beira-Rio'
-    }
+    with open('tratar_nomes_times.json', 'r') as f:
+        nomes_times = json.load(f)
 
     if time[0] in nomes_times:
-        return nomes_times[time[0]] + ' - ' + time[1]
+        try:
+            return nomes_times[time[0]] + ' - ' + time[1]
+        except Exception:
+            return nomes_times[time[0]]
 
-    return time[0] + ' - ' + time[1]
+    try:
+        return time[0] + ' - ' + time[1]
+    except Exception:
+        return time[0]
 
 
 def tratar_cidade(cidade):
     # Corrigir erros gramaticais
-    nomes_cidades = {
-        'Aguia Branca': 'Águia Branca',
-        'Sao Paulo': 'São Paulo',
-        'Bento Goncalves': 'Bento Gonçalves',
-        'Itajai': 'Itajaí',
-        'Braganca Paulista': 'Bragança Paulista',
-        'Goiania': 'Goiânia',
-        'Uberlandia': 'Uberlândia',
-        'Goianesia': 'Goianésia',
-        'Florianopolis': 'Florianópolis',
-        'Maceio': 'Maceió',
-        'São Luis': 'São Luís',
-        'Ribeirao Preto': 'Ribeirão Preto',
-        'Chapeco': 'Chapecó',
-        'Nova Iguacu': 'Nova Iguaçu',
-        'Niteroi': 'Niterói',
-        'Macapa': 'Macapá',
-        'Xanxere': 'Xanxerê',
-        'Maracanau': 'Maracanaú',
-        'Criciuma': 'Criciúma',
-        'Pocos de Caldas': 'Poços de Caldas',
-        'Rondonopolis': 'Rondonópolis',
-        'Joao Pessoa': 'João Pessoa',
-        'Belem': 'Belém',
-        'Brasilia': 'Brasília',
-        'Cacador': 'Caçador',
-        'Gravatai': 'Gravataí',
-        'Sao Jose dos Campos': 'São José dos Campos',
-        'Sao Leopoldo': 'São Leopoldo',
-        'Vitoria da Conquista': 'Vitória da Conquista',
-        'Jarinú': 'Jarinu',
-        'Cuiaba': 'Cuiabá',
-        'Sao Luis': 'São Luís',
-        'Sao Mateus do Maranhao': 'Sao Mateus do Maranhão',
-        'Patrocinio': 'Patrocínio',
-        'Jaragua': 'Jaraguá',
-        'Aparecida de Goiania': 'Aparecida de Goiânia',
-        'Paranagua': 'Paranaguá',
-        'Jaragua do Sul': 'Jaraguá do Sul',
-        'Paraiso do Tocantins': 'Paraíso do Tocantins',
-        'Ceara-Mirim': 'Ceará-Mirim',
-        'Varzea Grande': 'Várzea Grande',
-        'Sao Bernardo do Campo': 'São Bernardo do Campo',
-        'Tocantinopolis': 'Tocantinópolis',
-        'Monte Azul': 'Monte Azul Paulista',
-        'Nova Venecia': 'Nova Venécia',
-    }
+    with open('tratar_nomes_cidades.json', 'r') as f:
+        nomes_cidades = json.load(f)
 
     if cidade in nomes_cidades:
         return nomes_cidades[cidade]
@@ -119,34 +50,9 @@ def tratar_estadio(estadio):
     estadio = estadio.replace('Mun. ', '')
     estadio = estadio.replace('Dr. ', '')
     estadio = estadio.replace('Doutor ', '')
-    nomes_estadios = {
-        'Florestão': 'Arena da Floresta',
-        'Fonte Luminosa': 'Arena Fonte Luminosa',
-        'Manoel Barradas': 'Barradão',
-        'Ct Rei Pelé': 'CT Rei Pelé',
-        'CAT do Cajú': 'CAT do Caju',
-        'SESC Alterosas': 'Sesc Alterosas',
-        'CEFAT': 'Cefat',
-        'Estádio da Gávea': 'Gávea',
-        'SESC Campestre': 'Sesc Campestre',
-        'Do Café': 'Estádio do Café',
-        'Manoel Barretto': 'Barrettão',
-        'Ninho D´águia': "Ninho d'Águia",
-        'Jardim Inamar': 'Distrital do Inamar',
-        'Olimpio Perim': 'Olímpio Perim',
-        'Leão da Estradinha': 'Estradinha',
-        'Boca do Jacaré / Serejão': 'Serejão',
-        '1º de Maio': 'Primeiro de Maio',
-        'Jóia da Princesa': 'Joia da Princesa',
-        'Jose Batista Pereira Fernandes': 'Arena Inamar',
-        'Arena Allianz Parque': 'Allianz Parque',
-        'Urbano Caldeira': 'Vila Belmiro',
-        'Ademar Pereira de Barros': 'Arena Fonte Luminosa',
-        'Cícero Pompeu de Toledo': 'Morumbi',
-        'Orlando Batista Novelli': 'Arena Barueri',
-        'Oswaldo Teixeira Duarte': 'Canindé',
-        'Antonio Gomes Martins': 'Fortaleza',
-    }
+    
+    with open('tratar_nomes_estadios.json', 'r') as f:
+        nomes_estadios = json.load(f)
 
     if estadio in nomes_estadios:
         return nomes_estadios[estadio]
@@ -158,21 +64,27 @@ class SoccerGamesItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     nome_campeonato = Field(output_processor=Compose(TakeFirst(), str.strip))
+    
     time_mandante = Field(
         output_processor=Compose(TakeFirst(), str.strip, tratar_time)
     )
+    
     time_visitante = Field(
         output_processor=Compose(TakeFirst(), str.strip, tratar_time)
     )
+    
     estadio_jogo = Field(
         output_processor=Compose(TakeFirst(), str.strip, tratar_estadio)
     )
+    
     cidade_jogo = Field(
         output_processor=Compose(TakeFirst(), str.strip, tratar_cidade)
     )
+    
     estado_jogo = Field(output_processor=Compose(TakeFirst(), str.strip))
     data_jogo = Field(output_processor=Compose(TakeFirst(), str.strip))
     hora_jogo = Field(output_processor=Compose(TakeFirst(), str.strip))
+    jogo_adiado = Field(output_processor=TakeFirst())
     numero_jogo = Field(output_processor=TakeFirst())
     rodada_jogo = Field(output_processor=TakeFirst())
     fase_jogo = Field(output_processor=TakeFirst())
---
+++ soccer_games/soccer_games/spiders/flashscores_games.py
@@ -0,0 +1,152 @@
+import json
+from time import sleep
+
+import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.selector import Selector
+from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.common.by import By
+
+from soccer_games.items import SoccerGamesItem
+
+
+def obter_data_hora(data_hora):
+    # 05.03. 11:30
+    # "19.03. 12:00 - Adiado"
+    jogo_adiado = False
+    if 'Adiado' in data_hora:
+        jogo_adiado = True
+
+    data_hora = data_hora.split(' - ')[0]
+
+    data, hora = data_hora.split(' ')
+
+    data = data.split('.')
+    data.remove('')
+
+    if int(data[1]) < 7:
+        ano = 2022
+    else:
+        ano = 2021
+
+    data = f'{data[0]}-{data[1]}-{ano}'
+    return data, hora, jogo_adiado
+
+
+campeonatos = [
+    {'alemanha': ['bundesliga', 'Campeonato Alemão - Campeonato Alemão -']},
+    {'argentina': ['liga-profissional', 'Campeonato Argentino - Única']},
+    {'espanha': ['laliga', 'Campeonato Espanhol - Campeonato Espanhol -']},
+    {'franca': ['ligue-1', 'Campeonato Francês - Campeonato Francês -']},
+    {'inglaterra': ['campeonato-ingles', 'Campeonato Inglês - Única']},
+    {'italia': ['serie-a', 'Campeonato Italiano - Campeonato Italiano -']},
+    {
+        'portugal': [
+            'liga-portugal',
+            'Campeonato Português - Campeonato Português -',
+        ]
+    },
+]
+
+campeonatos = [
+    {'inglaterra': ['campeonato-ingles', 'Campeonato Inglês - Única']}
+]
+
+
+links = []
+nomes_campeonatos = []
+for campeonato in campeonatos:
+    for k, v in campeonato.items():
+        links.append(
+            f'https://www.flashscore.com.br/futebol/{k}/{v[0]}/calendario'
+        )
+        nomes_campeonatos.append(v[1])
+
+options = ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_argument('--headless')
+driver = Chrome('C:/chromedriver.exe', options=options)
+driver.maximize_window()
+
+
+class FlashscoresGamesSpider(scrapy.Spider):
+    name = 'flashscores_games'
+    allowed_domains = ['www.flashscore.com.br']
+    start_urls = ['https://www.flashscore.com.br']
+    f = open('../futebol_interior/flashscore_games.json', 'w').close()
+    def __init__(self):
+        self.html_lista = []
+        for link in links:
+            driver.get(link)
+            sleep(2)
+
+            try:
+                botao_popup = driver.find_element(
+                    By.ID, 'onetrust-accept-btn-handler'
+                )
+                botao_popup.click()
+            except:
+                pass
+
+            while True:
+                sleep(2)
+                try:
+                    botao_mais_jogos = driver.find_element(
+                        By.CLASS_NAME, 'event__more'
+                    )
+                    botao_mais_jogos.click()
+                except:
+                    break
+
+            self.html_lista.append(driver.page_source)
+        driver.close()
+
+    def parse(self, response):
+
+        for i in range(len(self.html_lista)):
+            resp = Selector(text=self.html_lista[i])
+            nome_campeonato = nomes_campeonatos[i]
+
+            jogos_site = resp.xpath(
+                "//div[@class='sportName soccer']//div[@class='event__round event__round--static' or @class='event__time' or @class='event__participant event__participant--home' or @class='event__participant event__participant--away']//text()"
+            ).getall()
+
+            todos_jogos = []
+            for i in range(len(jogos_site)):
+                if 'Adiado' in jogos_site[i]:
+                    continue
+                try:
+                    if 'Adiado' in jogos_site[i + 1]:
+                        jogos_site[i] = f'{jogos_site[i]} - Adiado'
+                except Exception:
+                    pass
+                todos_jogos.append(jogos_site[i])
+
+            c = 0
+            while c < len(todos_jogos):
+                jogo = ItemLoader(item=SoccerGamesItem(), selector=resp)
+                jogo.add_value('nome_campeonato', nome_campeonato)
+
+                if 'RODADA' in todos_jogos[c].upper():
+                    rodada = int(todos_jogos[c][-2::])
+                    c = c + 1
+                    continue
+
+                jogo.add_value('time_mandante', todos_jogos[c + 1])
+                jogo.add_value('time_visitante', todos_jogos[c + 2])
+
+                jogo.add_value('estadio_jogo', ' ')
+                jogo.add_value('cidade_jogo', ' ')
+                jogo.add_value('estado_jogo', ' ')
+
+                data, hora, jogo_adiado = obter_data_hora(todos_jogos[c])
+                jogo.add_value('data_jogo', data)
+                jogo.add_value('hora_jogo', hora)
+
+                jogo.add_value('jogo_adiado', jogo_adiado)
+
+                jogo.add_value('rodada_jogo', rodada)
+                jogo.add_value('fase_jogo', 'Única')
+
+                c = c + 3
+                yield jogo.load_item()
